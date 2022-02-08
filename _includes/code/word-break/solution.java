0023455b59b76aafa5c9a8c3ce4fb4e40603a242
evilj0ker <rajatsri94@gmail.com>
2022-02-09 04:15:34 +0530

    word break

---
+++ _includes/code/word-break/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) return false;
+
+        int n = s.length();
+        Set<String> dict = new HashSet<>(wordDict);
+        // dp[i] represents whether s[0...i] can be formed by dict
+        boolean[] dp = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                String sub = s.substring(j, i + 1);
+
+                if (dict.contains(sub) && (j == 0 || dp[j - 1])) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
